2d650ba2e65d9b0b371da87520fbfd7656fba10b
Machado117 <diogomachado@outlook.com>
2024-12-05 11:06:57 +0000

    feat(day03): Solve part 2

--- src/com/diogomachado/aoc24/Day03.java
+++ src/com/diogomachado/aoc24/Day03.java
@@ -10,7 +10,14 @@ import java.util.regex.Pattern;
 
 public class Day03 {
 
-    private final List<int[]> mulInstructions = new ArrayList<>();
+    private record MultiplyInstruction(int factorA, int factorB, boolean enabled) {
+
+        public long getProduct() {
+            return (long) factorA * factorB;
+        }
+    }
+
+    private final List<MultiplyInstruction> multiplications = new ArrayList<>();
 
     private Day03(final String input) {
         readInput(input);
@@ -19,13 +26,21 @@ public class Day03 {
     private void readInput(final String path) {
         try (final BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String line;
+            boolean instructionsEnabled = true;
             while ((line = reader.readLine()) != null) {
-                Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+                Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)|don't\\(\\)|do\\(\\)");
                 Matcher matcher = pattern.matcher(line);
-                while(matcher.find()) {
-                    mulInstructions.add(new int[]{Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2))});
+                while (matcher.find()) {
+                    String instruction = matcher.group(0).substring(0, matcher.group(0).indexOf("("));
+                    switch (instruction) {
+                        case "mul" -> multiplications.add(new MultiplyInstruction(Integer.parseInt(matcher.group(1)),
+                                                                                  Integer.parseInt(matcher.group(2)),
+                                                                                  instructionsEnabled
+                        ));
+                        case "do" -> instructionsEnabled = true;
+                        case "don't" -> instructionsEnabled = false;
+                    }
                 }
-
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -33,15 +48,17 @@ public class Day03 {
     }
 
     private long part1() {
-        return mulInstructions.stream().map(factors -> (long) factors[0] * factors[1]).reduce(0L, Long::sum);
+        return multiplications.stream().map(MultiplyInstruction::getProduct).reduce(0L, Long::sum);
     }
 
     private long part2() {
-        return -1;
+        return multiplications.stream()
+                              .filter(MultiplyInstruction::enabled)
+                              .map(MultiplyInstruction::getProduct)
+                              .reduce(0L, Long::sum);
     }
 
     public static void main(String[] args) {
-
         Day03 problem = new Day03("input/day03.txt");
 
         System.out.println(problem.part1());
